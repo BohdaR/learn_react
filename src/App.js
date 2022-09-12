e1f09f63c83a3c38b4155dfcc2884a021c99aeaa
BohdaR <bohdanshushval@gmail.com>
2022-09-12 17:01:48 +0300

    learnt functional and class components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+
+function Welcome(props) {
+  return <h1>Привіт, {props.name}</h1>;
+}
+
+// class Clock extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {date: new Date()};
+//     }
+//
+//     componentDidMount() {
+//         this.timerID = setInterval(
+//             () => this.tick(),
+//             1000
+//         );
+//     }
+//
+//     componentWillUnmount() {
+//         clearInterval(this.timerID);
+//     }
+//
+//     tick() {
+//         this.setState({
+//             date: new Date()
+//         });
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Привіт, світе!</h1>
+//                 <h2>Зараз {this.state.date.toLocaleTimeString()}.</h2>
+//             </div>
+//         );
+//     }
+// }
 
 function App() {
+    const [value, setValue] = useState('Some str')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h2>{value}</h2>
+          <input
+              type="text"
+              value={value}
+              onChange={event => setValue(event.target.value)}
+          />
+        <Welcome name="Василина" />
+        <Welcome name="Михайло" />
+        <Welcome name="Вадим" />
+          <Counter />
+          <ClassCounter />
+      </div>
   );
 }
 
